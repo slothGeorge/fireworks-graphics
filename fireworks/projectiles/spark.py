d5bb832cf9875f66e6906d91b97949d6c5402457
Rioran <voronov_rv@mail.ru>
2023-08-09 02:33:23 +0400

    refactor: spark color: made logic more plain

--- fireworks/projectiles/spark.py
+++ fireworks/projectiles/spark.py
@@ -3,14 +3,26 @@ from random import randint
 from projectiles.base_shape import Basic_Moving_Shape
 
 
+COLOR_BOUNDS = (100, 255)
+
+
 class Spark(Basic_Moving_Shape):
     def __init__(self, x, y):
         super().__init__()
-        color = [randint(100,255), randint(100,255), randint(100,255)]
-        self.color(f"#{''.join(f'{hex(i)[2:].upper():0>2}' for i in color)}")
+        self.color(self.get_random_color())
         self.goto(x,y)
         self.resizemode("user")
         self.shapesize(randint(1,4), randint(1,4))
         self.y_speed = randint(20, 50)
         self.x_speed = randint(-20, 20)
         self.countdown = randint(7, 21)
+    
+    @staticmethod
+    def get_random_color():
+        color_parts = ['#']
+        color_parts += [
+            hex(randint(*COLOR_BOUNDS))[2:]  # to chop off starting '0x' part
+            for _ in range(3)
+        ]
+        colors_text = ''.join(color_parts)
+        return colors_text
