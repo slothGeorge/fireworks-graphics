76bbac28e0c9f50c0d2c6b45e90de18c3f916e3f
slothGeorge <gosh20002@gmail.com>
2024-04-23 21:51:55 +0400

    adding flag s
    adding flag s: setting range of spark

--- fireworks/game.py
+++ fireworks/game.py
@@ -6,11 +6,21 @@ from projectiles.rocket import Rocket
 from projectiles.spark import Spark
 
 
+def draw_text(text, x, y, fill="black"):
+    my_text = turtle.Turtle()
+    my_text.speed(0)
+    my_text.penup()
+    my_text.goto(x, y)
+    my_text.color(fill)
+    my_text.write(text, align='center', font=('Verdana', 20, 'normal'))
+    my_text.hideturtle()
+
+
 class Game:
 
-    def __init__(self):
-        self.projectiles: list  = list()
-
+    def __init__(self, amount_of_sparks_range=(20, 30)):
+        self.projectiles: list = list()
+        self.amount_of_sparks_range = amount_of_sparks_range
         turtle.setup(800, 600)
         turtle.onkey(self.exit, "Escape")
         turtle.onkey(self.rocket_launch, "space")
@@ -21,16 +31,7 @@ class Game:
         self.window.title("Rio fireworks demo animation")
         self.window.bgcolor("#13008D")
 
-        self.draw_text('Press SPACE to firework // Press ESC to close', 0, 250, '#FFFFFF')
-
-    def draw_text(self, text, x, y, fill="black"):
-        my_text = turtle.Turtle()
-        my_text.speed(0)
-        my_text.penup()
-        my_text.goto(x, y)
-        my_text.color(fill)
-        my_text.write(text, align='center', font=('Verdana', 20, 'normal'))
-        my_text.hideturtle()
+        draw_text('Press SPACE to firework // Press ESC to close', 0, 250, '#FFFFFF')
 
     def animate(self):
         self.window.update()
@@ -61,7 +62,7 @@ class Game:
     
     def explode_rocket(self, rocket):
         coordinates = (rocket.xcor(), rocket.ycor())
-        for _ in range(randint(20, 30)):
+        for _ in range(randint(*self.amount_of_sparks_range)):
             spark = Spark(*coordinates)
             self.projectiles.insert(0, spark)
 
--- fireworks/main.py
+++ fireworks/main.py
@@ -1,6 +1,18 @@
+import argparse
 from game import Game
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sparks-amount-range', '-s', type=str, default='20-30',
+                        help='Sets a range amount of sparks. Min=20, Max=30')
+    args = parser.parse_args()
+
+    amount_of_sparks_range = tuple(int(num) for num in args.amount_of_sparks_range.split('-'))
+    return amount_of_sparks_range
+
+
 if __name__ == '__main__':
+    parse_args()
     application = Game()
     application.run()
